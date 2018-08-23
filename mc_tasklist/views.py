ebf293b0a28d3befa674fac3593fe4e315a02da4
Sascha <sascha.trifunovic@gmail.com>
2018-08-23 10:49:38 +0200

    Add caching with MemCachier

--- mc_tasklist/views.py
+++ mc_tasklist/views.py
@@ -1,9 +1,17 @@
 from django.template.context_processors import csrf
 from django.shortcuts import render_to_response, redirect
 from mc_tasklist.models import Task
+from django.core.cache import cache
+import time
+
+TASKS_KEY = "tasks.all"
 
 def index(request):
-    tasks = Task.objects.order_by("id")
+    tasks = cache.get(TASKS_KEY)
+    if not tasks:
+        time.sleep(2)  # simulate a slow query.
+        tasks = Task.objects.order_by("id")
+        cache.set(TASKS_KEY, tasks)
     c = {'tasks': tasks}
     c.update(csrf(request))
     return render_to_response('index.html', c)
