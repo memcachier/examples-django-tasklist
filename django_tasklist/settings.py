9869346743374a8f56d3a514cb299c107f2941e9
Paddy O'Hanlon <paddy@paddyohanlon.com>
2019-11-26 15:55:58 +0100

    Use `python-binary-memcached` instead of `pylibmc`
    Update dependencies also.

--- django_tasklist/settings.py
+++ django_tasklist/settings.py
@@ -132,33 +132,15 @@ def get_cache():
     password = os.environ['MEMCACHIER_PASSWORD']
     return {
       'default': {
-        'BACKEND': 'django.core.cache.backends.memcached.PyLibMCCache',
+        'BACKEND': 'django_bmemcached.memcached.BMemcached',
         # TIMEOUT is not the connection timeout! It's the default expiration
         # timeout that should be applied to keys! Setting it to `None`
         # disables expiration.
         'TIMEOUT': None,
         'LOCATION': servers,
         'OPTIONS': {
-          'binary': True,
           'username': username,
           'password': password,
-          'behaviors': {
-            # Enable faster IO
-            'no_block': True,
-            'tcp_nodelay': True,
-            # Keep connection alive
-            'tcp_keepalive': True,
-            # Timeout settings
-            'connect_timeout': 2000, # ms
-            'send_timeout': 750 * 1000, # us
-            'receive_timeout': 750 * 1000, # us
-            '_poll_timeout': 2000, # ms
-            # Better failover
-            'ketama': True,
-            'remove_failed': 1,
-            'retry_timeout': 2,
-            'dead_timeout': 30,
-          }
         }
       }
     }
